196384dc1c28dc0459d93a75001e550ff87f7752
Tomislav Čović <tcovic01@fesb.hr>
2021-03-31 12:48:15 +0200

    Dodao sam dvije linije.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Novo");
+            Console.WriteLine("Staro");
         }
     }
 }
