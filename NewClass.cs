8f78a649b96971014c4696ceb8024dc4150b47db
Tomislav Čović <tcovic01@fesb.hr>
2021-04-01 08:58:21 +0200

    Merged

--- NewClass.cs
+++ NewClass.cs
@@ -6,13 +6,9 @@ namespace ConsoleApp1
 {
     class NewClass
     {
-<<<<<<< HEAD
         static void PraznaMetoda()
         {
-
+            Console.WriteLine("Druga promjena");
         }
-=======
-        Console.WriteLine("Druga promjena");
->>>>>>> 7c4d34c20a1ba67f608f13cf7d9b7d878ca11224
     }
 }
